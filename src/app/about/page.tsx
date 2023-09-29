c8b1c3f7c79efb1584cd86d1ed9815048fce0f13
harshil-063 <hk.bitcoding@gmail.com>
2023-09-29 14:32:48 +0530

    feat: start project --initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  // Your component code here
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
